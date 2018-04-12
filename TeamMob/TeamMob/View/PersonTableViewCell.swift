721c59ead416b9201f541985aa34e4e63e19a8f6
Cleopas <johndoe@jhb.dvt.co.za>
2018-04-12 12:01:09 +0200

    Implememented a circlular image view in the Table View

--- TeamMob/TeamMob/View/PersonTableViewCell.swift
+++ TeamMob/TeamMob/View/PersonTableViewCell.swift
@@ -8,22 +8,34 @@
 
 import UIKit
 
-class PersonTableViewCell: UITableViewCell {
+class PersonTableViewCell: UITableViewCell{
     
     //MARK: Properties
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var jobTitleLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var projectLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        let radius = profileImageView.frame.width/2
+        profileImageView.layer.cornerRadius = radius
+        profileImageView.layer.masksToBounds = true
+        
     }
+    
+
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
 }
+    
+
+
+
+
+
